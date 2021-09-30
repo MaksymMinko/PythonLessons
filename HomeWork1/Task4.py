e189b600212916ca07685773f97545685ddbb106
MaksymMinko <mkmin@ukr.net>
2021-09-30 13:44:42 +0300

    Update Task4.py

---
+++ HomeWork1/Task4.py
@@ -0,0 +1,23 @@
+num = int(input('Enter the number: '))
+if num == 1:
+    print('1 = I')
+elif num == 2:
+    print('2 = II')
+elif num == 3:
+    print('3 = III')
+elif num == 4:
+    print('4 = IV')
+elif num == 5:
+    print('5 = V')
+elif num == 6:
+    print('6 = VI')
+elif num == 7:
+    print('7 = VII')
+elif num == 8:
+    print('8 = VIII')
+elif num == 9:
+    print('2 = IX')
+elif num == 10:
+    print('10 = X')
+else:
+    print('ERROR')
