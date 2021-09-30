b75e433d68387726397b6df21444654cea93466c
MaksymMinko <mkmin@ukr.net>
2021-09-30 13:27:18 +0300

    Update Task1.py

---
+++ HomeWork1/Task1.py
@@ -0,0 +1,17 @@
+wd = input('Enter the day of the week: ')
+if wd == '1':
+    print('Monday')
+elif wd == '2':
+    print('Tuesday')
+elif wd == '3':
+    print('Wednesday')
+elif wd == '4':
+    print('Thursday')
+elif wd == '5':
+    print('Friday')
+elif wd == '6':
+    print('Saturday')
+elif wd == '7':
+    print('Sunday')
+else:
+    print('Incorrect data')
